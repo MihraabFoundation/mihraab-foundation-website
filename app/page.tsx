d68f36f7d6591accb11a7e18d932a186ea1da00d
Umair Qureshi <umairq96@uw.edu>
2025-12-14 10:50:39 -0800

    re-org

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import HeroSection from "@/components/hero-section"
-import MissionBranchesSection from "@/components/mission-branches-section"
+import MissionBranchesSection from "@/components/mission-branches/mission-branches-section"
 import EventsCarousel from "@/components/events-carousel"
 import Footer from "@/components/footer"
 
